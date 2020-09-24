f683a5c50c49c7d564df84037a242bc514cfbf0b
Alireza Hajebrahimi <mrex778@yahoo.ca>
2020-09-24 21:37:21 +0900

    Initial Commit

---
+++ ReWant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReWant
+//
+//  Created by Alireza Hajebrahimi on 2020/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
